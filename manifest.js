b061377d5aeee1fc659bac143ac2af997309106b
Erick Patrick <github@erickpatrick.net>
2017-04-29 20:43:01 +0200

    bumped version and added new entries to permissions

--- manifest.js
+++ manifest.js
@@ -1,13 +1,17 @@
 {
     "name": "Tuts+ Publishing",
-    "version": "0.5",
+    "version": "0.7",
     "manifest_version": 2,
     "description": "Make speedster",
     "background": {
         "scripts": ["background.js"]
     },
     "content_scripts": [{
-        "matches": ["https://tutsplus.io/*", "https://*.tutsplus.com/*"],
+        "matches": [
+            "https://tutsplus.io/*",
+            "https://*.tutsplus.com/*",
+            "https://getnative.me/*"
+        ],
         "js": ["content.js"],
         "run_at": "document_end"
     }],
