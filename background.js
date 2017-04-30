7b2aafae3dd32f7789beca6ee0ad55881210b784
Erick Patrick <github@erickpatrick.net>
2017-04-30 03:04:05 +0200

    update background.js to accept multiple messages

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 // needed as content_scripts don't have access to chrome.tabs api
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    chrome.tabs.create(msg);
-})
+    if (msg.name == 'new-tab') {
+        chrome.tabs.create(msg.payload);
+    }
+});
