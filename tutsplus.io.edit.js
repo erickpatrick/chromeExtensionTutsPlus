0c57cc933a60ce8db5f6e23274f4232cd5bf45f9
Erick Patrick <github@erickpatrick.net>
2017-04-30 04:12:42 +0200

    open existing translation page otherwise new translation page

--- tutsplus.io.edit.js
+++ tutsplus.io.edit.js
@@ -1,22 +1,50 @@
 // if on edit page, go directly to translate page
 if (window.location.href.search('edit') !== -1) {
-    let openTranslationPage = function () {
-        document.querySelector('a.translation-list__add-translation-button.btn').click();
-    };
-
     // opens original publication in a new tab so we can grab the new translation url
     // but keeps focus on the current one
-    let goToOriginalPost = function () {
+    let goToOriginalTutsPlusComPost = function () {
         // content_scripts don't have access to chrome.tabs, so we need to send a
         // message to a background script so it can open the tab for us
         openUrlInNewTab(document.querySelector('.post-form__url a').getAttribute('href'));
     };
 
+    // if post has translation returns it, otherwise, null
+    let getTranslation = function () {
+        let publication = JSON.parse(Cookie.get('publication') || JSON.stringify({}));
+        let translationList = document.querySelectorAll('.translation-list__language-name');
+        let translation = null;
+
+        // has translation?
+        [].filter.call(translationList, function (item) {
+            if (item.innerText == publication.language) {
+                translation = item;
+            }
+        });
+
+        return translation;
+    }
+
+    // opens original post on tutsplus.com then if post has translation for this 
+    // language open it otherwise  open add translation page
+    let openTranslationPage = function () {
+        let postTranslation = getTranslation();
+
+        goToOriginalTutsPlusComPost();
+
+        // exists or new?
+        if (postTranslation) {
+            postTranslation.parentElement.click();
+        } else {
+            document.querySelector('a.translation-list__add-translation-button.btn').click();
+        }
+    };
+
+    // automatically opens translation page
     if (window.location.href.search('automatic') !== -1) {
-        goToOriginalPost();
         openTranslationPage();
     }
 
+    // add buttons to the bar
     bar.appendChild(createButtonAndAttachMethod('Translate', openTranslationPage));
-    bar.appendChild(createButtonAndAttachMethod('Original', goToOriginalPost));
+    bar.appendChild(createButtonAndAttachMethod('Original', goToOriginalTutsPlusComPost));
 }
