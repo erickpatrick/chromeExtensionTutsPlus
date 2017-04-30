f678eabd9323dd2626aaa299e8d35df3ac0630a0
Erick Patrick <github@erickpatrick.net>
2017-04-30 04:09:48 +0200

    add comments to explain things

--- helper.js
+++ helper.js
@@ -1,3 +1,4 @@
+// URL for get request of search page from tutsplus.io
 let createSearchUrl = function (term) {
     return 'https://tutsplus.io/posts/searches?utf8=%E2%9C%93&post_search%5Bname_query_filter%5D=' +
         encodeURI(term) + '&post_search%5Bstatus_filter%5D=&post_search%5Btopic_filter%5D=&commit=Filter';
@@ -14,10 +15,13 @@ let createButtonAndAttachMethod = function (btnText, btnFunction) {
     return button;
 };
 
+// some actions are not allowed on context_scripts of chrome extensions, thus
+// we need to send messages to backgroun script so it can do the work
 let sendMessageToBackground = function (msg) {
     chrome.runtime.sendMessage(msg, function () {});
 }
 
+// wrapper for opening a new tab around sendMessageToBackground
 let openUrlInNewTab = function (url) {
     sendMessageToBackground({
         name: 'new-tab',
@@ -28,7 +32,8 @@ let openUrlInNewTab = function (url) {
     })
 }
 
-let Cookie = {
+// immutable Object the deals with Cookies
+const Cookie = {
     set: function (name, value, exdays = 1) {
         let d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -55,13 +60,9 @@ let Cookie = {
         }
 
         return "";
-    },
-
-    delete: function (name) {
-        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     }
 }
 
+// initiate bar
 let bar = document.createElement('div');
-
 bar.setAttribute('class', 'chrExtBar');
