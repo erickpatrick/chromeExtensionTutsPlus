0deba9428b988bb76144ef855f8129e5982900e2
Erick Patrick <github@erickpatrick.net>
2017-05-03 11:36:46 +0200

    don't show extra information if it's not billable

--- tutsplus.com.js
+++ tutsplus.com.js
@@ -1,27 +1,31 @@
+// Only do this if it's google spreadsheets
 if (window.location.href.search('tutsplus.com\/tutorials\/') !== -1 ||
     window.location.href.search('tutsplus.com\/articles\/') !== -1) {
     let billable = document.querySelector("meta[property='native:billable']").getAttribute('content');
     let contentLength = document.querySelector("meta[property='native:length']").getAttribute('content');
     let div = document.createElement('div');
     let extraWork = 0;
-    let text = '<p><strong>Billable?</strong> ' + billable + ' <strong>Length?</strong> ' + contentLength + ' <strong>Money?</strong> ';
+    let text = '<p><strong>Billable?</strong> ' + billable;
 
+    if (billable !== 'false') {
+        text += ' <strong>Length?</strong> ' + contentLength + ' <strong>Money?</strong> ';
+    }
 
     // is it a small publication?
-    if (contentLength < 1300) {
+    if (billable !== 'false' && contentLength < 1300) {
         text += 9;
         text += '</p>';
     }
 
     // is it a medium publication and how much extra work?
-    if (contentLength >= 1300 && contentLength <= 2100) {
+    if (billable !== 'false' && contentLength >= 1300 && contentLength <= 2100) {
         extraWork = contentLength - 1300;
         text += 13 + ' <strong>Extra?</strong> ' + extraWork;
         text += '</p>';
     }
 
     // is it a big publication and how much extra work?
-    if (contentLength > 2100) {
+    if (billable !== 'false' && contentLength > 2100) {
         extraWork = contentLength - 2100;
         text += 18 + ' <strong>Extra?</strong> ' + extraWork;
         text += '</p>';
